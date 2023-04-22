b245e4feeba4b4dc3dc2281c31e35d639b4e1c90
Dportocarrero19 <por19002@byui.edu>
2023-04-22 19:35:32 -0400

    Index site finished

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+let x = document.lastModified;
+document.getElementById("updated").innerHTML = "Last updated: " + x;
+
+let currentYear = new Date().getFullYear();
+document.querySelector("span").innerHTML = currentYear
+
+
