f85c0b6a3b8c975612195057a998389d8337d17a
Dportocarrero19 <por19002@byui.edu>
2023-05-08 21:13:05 -0400

    First stage

---
+++ chamber/scripts/chamber.js
@@ -0,0 +1,25 @@
+
+let currentYear = new Date().getFullYear();
+document.querySelector(".currentyear").innerHTML = currentYear;
+
+let lastModified = document.lastModified;
+document.querySelector(".updated").innerHTML = lastModified;
+
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let d = new Date();
+let day = days[d.getDay()];
+
+let numdate = d.getDate();
+
+
+let month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+let months = month[d.getUTCMonth()];
+
+
+let year = d.getFullYear();
+
+let dates = day + ",  " + numdate + " " + months + " " + year;
+
+
+document.querySelector(".date").innerHTML= dates;
+
