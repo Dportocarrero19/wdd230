c98488bccfb41d3b71cf292a9435a9f719df0537
Dportocarrero19 <por19002@byui.edu>
2023-05-16 22:07:25 -0400

    week 5

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -26,3 +26,11 @@ document.querySelector(".date").innerHTML= dates;
 function toggleMenu() {
     document.getElementsByClassName("navigation-list")[0].classList.toggle("responsive");
 }
+
+if (day == 'Monday'||day == 'Tuesday'){
+    const meeting = document.querySelector('div');
+    meeting.innerHTML = '🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.';
+
+    const contain = document.getElementById('meeting-time')
+    contain.appendChild(meeting);
+}
