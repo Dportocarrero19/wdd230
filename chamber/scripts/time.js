fc35287043a272819efee0ca1ecb3bdc1891fb25
Dportocarrero19 <por19002@byui.edu>
2023-06-09 22:12:23 -0400

    discover

--- chamber/scripts/time.js
+++ chamber/scripts/time.js
@@ -1,3 +1,17 @@
+let date = new Date();
+
+let stampdate = new Date();
 
 
-localStorage.setItem(new Date(), "date")
+localStorage.setItem("localdate", stampdate.getTime());
+
+let date2 = date.getTime();
+
+let dateday = localStorage.getItem("localdate");
+
+let difference = dateday-date2;
+let diff = difference / (1000 * 3600 * 24)
+
+document.querySelector(".time").innerHTML = diff;
+
+
