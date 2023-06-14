ecac9bb4f12e615910583cb0926379fd2903e876
Dportocarrero19 <por19002@byui.edu>
2023-06-14 18:26:18 -0400

    comments

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -14,31 +14,53 @@ const display = (Businesseses) => {
         // Create elements to add to the div.cards element
         let card = document.createElement('section');
         let h2 = document.createElement('h2');
-        let birth = document.createElement('p');
-        let place = document.createElement('p');
+        let adrs = document.createElement('p');
+        let phone = document.createElement('p');
+        phone.className = "phone";
+        let member = document.createElement('p');
+        member.className = "member";
         let photo = document.createElement('img');
-        
+        let websiteurl = document.createElement('p');
     
         // Build the h2 content out to show the prophet's full name - finish the template string
-        h2.textContent = `${prophet.name} ${prophet.lastname}`;
-        birth.textContent = `Date of Birth: ${Businesses.address}`;
-        place.textContent = `Place of Birth: ${prophet.phonenumber}`;
+        h2.textContent = `${Businesses.name}`;
+        member.textContent = `${Businesses.membershiplevel}`
+        adrs.textContent = `${Businesses.address}`;
+        phone.textContent = `${Businesses.phonenumber}`;
+        websiteurl.textContent = `${Businesses.websiteurl}`
+        member.textContent= `${Businesses.membershiplevel} member`
         // Build the image portrait by setting all the relevant attribute
         photo.setAttribute('src', Businesses.image);
-        photo.setAttribute('alt',`${Businesses.address}`);
-        photo.setAttribute('alt',`${prophet.phonenumber}`);
-        photo.setAttribute('alt', `Portait of ${prophet.name} ${prophet.lastname}`);
         photo.setAttribute('loading', 'lazy');
-        photo.setAttribute('width', '340');
-        photo.setAttribute('height', '440');
-        
+       
         
         // Append the section(card) with the created elements
-        card.appendChild(h2);
-        card.appendChild(birth);
-        card.appendChild(place);
         card.appendChild(photo);
+        card.appendChild(h2);
+        card.appendChild(websiteurl);
+        card.appendChild(adrs);
+        card.appendChild(phone);
+        card.appendChild(member)
+        
 
         cards.appendChild(card);
     }) // end of forEach loop
 }; // end of function expression
+
+
+const gridbut = document.querySelector("#grid")
+const listbut = document.querySelector("#list")
+const displaybut = document.querySelector(".cards")
+
+gridbut.addEventListener("click", () => {
+	// example using arrow function
+	displaybut.classList.add("grid");
+	displaybut.classList.remove("list");
+});
+
+listbut.addEventListener("click", showList); // example using defined function
+
+function showList() {
+	displaybut.classList.add("list");
+	displaybut.classList.remove("grid");
+}
--- lesson9/scripts/prophets.js
+++ lesson9/scripts/prophets.js
@@ -2,17 +2,15 @@ const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophet
 async function getProphetData() {
     const response = await fetch(url);
     const data = await response.json();
-    //console.table(data.prophets);  // note that we reference the prophet array of the data object given the structure of the json file
     displayProphets(data.prophets);
 };
 
 getProphetData();
 
 const displayProphets = (prophets) => {
-    const cards = document.querySelector('div.cards'); // select the output container element
+    const cards = document.querySelector('div.cards'); 
   
     prophets.forEach((prophet) => {
-        // Create elements to add to the div.cards element
         let card = document.createElement('section');
         let h2 = document.createElement('h2');
         let birth = document.createElement('p');
@@ -20,26 +18,21 @@ const displayProphets = (prophets) => {
         let portrait = document.createElement('img');
         
     
-        // Build the h2 content out to show the prophet's full name - finish the template string
         h2.textContent = `${prophet.name} ${prophet.lastname}`;
         birth.textContent = `Date of Birth: ${prophet.birthdate}`;
         place.textContent = `Place of Birth: ${prophet.birthplace}`;
-        // Build the image portrait by setting all the relevant attribute
         portrait.setAttribute('src', prophet.imageurl);
-        portrait.setAttribute('alt',`${prophet.birthdate}`);
-        portrait.setAttribute('alt',`${prophet.birthplace}`);
         portrait.setAttribute('alt', `Portait of ${prophet.name} ${prophet.lastname}`);
         portrait.setAttribute('loading', 'lazy');
         portrait.setAttribute('width', '340');
         portrait.setAttribute('height', '440');
         
         
-        // Append the section(card) with the created elements
         card.appendChild(h2);
         card.appendChild(birth);
         card.appendChild(place);
         card.appendChild(portrait);
 
         cards.appendChild(card);
-    }) // end of forEach loop
-}; // end of function expression
+    }) 
+}; 
