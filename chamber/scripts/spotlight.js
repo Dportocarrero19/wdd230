a832c730743425141ffd7a4cd19a8d9056092f56
Dportocarrero19 <por19002@byui.edu>
2023-06-29 21:05:23 -0400

    icons

---
+++ chamber/scripts/spotlight.js
@@ -0,0 +1,37 @@
+const titleh2 = document.querySelector('#spotlight-h2');
+const titleimage = document.querySelector('#spotlight-image');
+
+
+const urlj = 'https://dportocarrero19.github.io/wdd230/chamber/json/data.json';
+async function apiFetch() {
+    try {
+        const response = await fetch(urlj);
+        if (response.ok) {
+          const data = await response.json();
+  
+          console.log(data); 
+  
+          displayResults(data);
+  
+        } else {
+            throw Error(await response.text());
+        }
+      } catch (error) {
+  
+        console.log(error);
+      }
+    }
+apiFetch();
+
+
+
+function displayResults(Business) {
+    titleh2.innerHTML = `${Business.Businesses[1].name}`;
+    const icon =  `${Business.Businesses[1].image}`;
+
+    const iconsrc = `https://dportocarrero19.github.io/wdd230/chamber/images/${Business.Businesses[1].icon}.jpg`;
+
+    titleimage.setAttribute('src', iconsrc);
+
+}
+
